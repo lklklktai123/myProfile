21854c957cf836b09ed91768030f158fcf1959bb
Phan Tai <tai.phan@levincigroup.com>
2021-05-27 10:11:58 +0700

    complete header

--- my-profile/src/App.js
+++ my-profile/src/App.js
@@ -1,8 +1,10 @@
+import About from './components/About/about';
 import Header from './components/Header/header';
 function App() {
   return (
     <div className="container">
       <Header />
+      <About />
     </div>
   );
 }
---
+++ my-profile/src/components/About/about.js
@@ -0,0 +1,8 @@
+const about = () => {
+  return (
+    <section className="about">
+      <h3 className="heading-3">About</h3>
+    </section>
+  );
+};
+export default about;
