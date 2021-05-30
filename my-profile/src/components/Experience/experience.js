0f90156bd02daa4cca41aec409f411418cac8ff9
Phan Tai <tai.phan@levincigroup.com>
2021-05-30 10:04:02 +0700

    experient add content

--- my-profile/src/components/Experience/experience.js
+++ my-profile/src/components/Experience/experience.js
@@ -8,8 +8,18 @@ const experience = () => {
         Hay de toi gioi thieu mot so trai nghiem ve ban than minh
       </h4>
       <div className="experience__timeline">
+        <div className="experience__timeline-line"></div>
         <div className="experience__timeline-block"></div>
-        <div className="experience__timeline-content"></div>
+        <div className="experience__timeline-content">
+          <h4 className="project-title">Envato Ltd.</h4>
+          <p className="normal-text position-title">Software Engineer</p>
+          <p className="normal-text ">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto,
+            optio, dolorum provident rerum aut hic quasi placeat iure tempora
+            laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis
+            qui ut.
+          </p>
+        </div>
       </div>
     </section>
   );
